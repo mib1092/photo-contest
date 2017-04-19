1fc8ab7f18ee23bb46e811bcaf4847307b98e6a8
Anton <suddyadred@gmail.com>
2017-04-19 12:38:06 +0300

    suddya

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($) {
         selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
         speed: 500, // Integer. How fast to complete the scroll in milliseconds
         easing: 'easeInQuad', // Easing pattern to use
-        offset: 50 // Integer. How far to offset the scrolling anchor location in pixels
+        offset: 0 // Integer. How far to offset the scrolling anchor location in pixels
     });
 
     // $(window).on('load', function() {
